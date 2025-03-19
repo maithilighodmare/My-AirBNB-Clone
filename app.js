09c64b34d1e5d336b45053f6d88bf35b3ca42bca
maithilighodmare26 <165919705+maithilighodmare26@users.noreply.github.com>
2025-03-19 23:23:13 +0530

    added phase 1 part a

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust"
 
@@ -19,6 +20,7 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.send("Hi! I am root.")
@@ -28,12 +30,42 @@ app.get("/listings", async(req,res)=>{
  const allListings = await Listing.find({});
  res.render("listings/index.ejs",{allListings});
 });
-
+// New Route
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+})
 // Show Route
 app.get("/listings/:id", async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+});
+// Create route
+app.post("/listings" , async (req,res)=>{
+
+   const newListing = new Listing(req.body.listing);
+   await newListing.save();
+   res.redirect("/listings");
+});
+// Edit Route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+// Update route
+
+app.put("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+// Delete Route
+app.delete("/listings/:id", async (req,res)=>{
+    let {id} = req.params;
+  let deletedListings = await Listing.findByIdAndDelete(id);
+  console.log(deletedListings);
+  res.redirect("/listings");
 })
 // app.get("/testlistings" , async(req,res) =>{
 //  let sampleListing = new  Listing({
